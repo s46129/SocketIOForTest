82b6660187a79eb1fdb2d8dab500882ef2d89734
ZhiYuan <s46129@yahoo.com.tw>
2025-02-25 19:10:26 +0800

    Update RunServer.bat

--- RunServer.bat
+++ RunServer.bat
@@ -9,13 +9,9 @@ if not exist "node_modules" (
 )
 
 REM 利用 netstat 檢查 80 埠是否被使用
-netstat -aon | findstr ":80 " >nul
-if %errorlevel%==0 (
-    echo Port 80 server already used.
-	pause
-) else (
+
     echo Initial server...
     REM 在新視窗中啟動 server
     start node SocketIOServer.js
-)
+
 
